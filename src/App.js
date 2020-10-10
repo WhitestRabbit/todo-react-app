693fad9ead04d05cc67c161d007d8492a533a6b4
WhitestRabbit <stefanosmixalas@gmail.com>
2020-10-10 19:11:19 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (<div>
+                <label className="red">Option 1</label><input type="checkbox" />
+                <label>Option 2</label><input type="checkbox" />
+                <label>Option 3</label><input type="checkbox" />
+            </div>);
 }
 
 export default App;
